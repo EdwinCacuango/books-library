b2380f99c5db13073521bb6365f1ca9419c1f17d
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-09 10:31:50 -0500

    Render books that are saved in session

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useAppContext } from "../components/Store";
+import Layout from "../components/layout";
+import Book from "../components/book";
 
 const Home = () => {
-  return <div>Home</div>;
+  const store = useAppContext();
+  return (
+    <Layout>
+      {store.items.map((item) => (
+        <Book book={item} key={item.id}></Book>
+      ))}
+    </Layout>
+  );
 };
 
 export default Home;
