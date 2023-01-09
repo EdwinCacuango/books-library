dd93f2233637ac286749fe519d0c87ae2f1b7d1d
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-09 15:20:03 -0500

    Styles for add book page and search bar - Added react icon package - In navbar, added a search bar without functions - In add book page:     - Added an img to the right section     - Styled accordin navbar colors     - Changed p tag for label tags     - Hidden file input and function leave to their label

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <div className="bg-white min-h-screen lg:min-w-[60rem] rounded-md drop-shadow-lg">
+      <nav className="bg-orange-500 min-h-[4rem] py-4 px-8 rounded-t-md">
+        <ul className="flex gap-10 text-xl text-white font-medium justify-between items-center">
+          <li className="min-w-[18rem]">
+            <Link to="/add-book">Add a book</Link>
           </li>
-          <li>
-            <Link to="/add-book">New Book</Link>
+          <li className="self-center text-center min-w-[16rem]">
+            <NavLink to="/" className=" hover:active-white active:font-white">
+              Kruger Library
+            </NavLink>
+          </li>
+          <li className="bg-white flex items-center justify-between px-1 rounded py-1 min-w-[16rem]">
+            <input
+              placeholder="Search a book"
+              className="p-1 rounded-md text-base font-light min-w-[16rem]"
+            />
+            <button className="text-black bg-transparent p-1">
+              <AiOutlineSearch />
+            </button>
           </li>
         </ul>
       </nav>
-      {children}
+      <div>{children}</div>
     </div>
   );
 };
