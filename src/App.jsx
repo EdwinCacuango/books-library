f2af0bcf095fb77f7d891c61bf254fba313dd9b0
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-09 08:24:44 -0500

    Added pages elements and routes to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+import OneBook from "./pages/OneBook";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-book" element={<AddBook />} />
+        <Route path="/view/:bookId" element={<OneBook />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
