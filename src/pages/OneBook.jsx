e0f085fa79a4dd7e0aa07bc935c4c74c20640eb5
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-09 10:32:18 -0500

    One book component - Search a book according to id - Render information if the element is saven in local session     - Show main info of book searched

--- src/pages/OneBook.jsx
+++ src/pages/OneBook.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../components/layout";
+import { useAppContext } from "../components/Store";
 
 const OneBook = () => {
-  return <div>OneBook</div>;
+  const [book, setBook] = useState({});
+  const params = useParams();
+  const store = useAppContext();
+
+  useEffect(() => {
+    const BOOK = store.getItem(params.bookId);
+    setBook(BOOK);
+  }, []);
+
+  if (!book) {
+    return <div>Item not found</div>;
+  }
+  return (
+    <Layout>
+      <h1>{book.title}</h1>
+      <div>{book.cover ? <img src={book.cover} width="400" /> : ""}</div>
+      <div>{book.author}</div>
+      <div>{book.intro}</div>
+      <div>{book.completed ? "Completado" : "No terminado"}</div>
+      <div>{book.review}</div>
+    </Layout>
+  );
 };
 
 export default OneBook;
